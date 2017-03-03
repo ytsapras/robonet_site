c226de5addc93b1a5c76627c2783da9a53d8908d
ytsapras <huluvu38@gmail.com>
2017-03-03 16:52:41 +0100

    added artemis cols

--- scripts/local_conf.py
+++ scripts/local_conf.py
@@ -2,7 +2,7 @@ import os
 def get_conf(request):
    paths = {'artemis':os.getcwd()+'/artemis/',
             'robonet_site':os.getcwd()+'../robonet_site/',
-	    'artemis_cols':os.getcwd()+'artemis/',
+	    'artemis_cols':os.getcwd()+'/artemis/',
 	    'site_url':'http://127.0.0.1:8000/'
 	    }
    answer = paths[request] 
