fd8b6c50ea6bd3d316482aed29bfdf41b29cfd0a
rachel3834 <rstreet@lcogt.net>
2017-03-18 12:39:57 +0100

    Adding testing code, starting with the config parser, so also added an example config for obscontrol

---
+++ scripts/tests/test_configs.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 18 12:16:03 2017
+
+@author: rstreet
+"""
+
+import unittest
+from os import getcwd, path
+from sys import path as systempath
+cwd = getcwd()
+systempath.append(path.join(cwd,'..'))
+import config_parser
+
+class TestConfigParser(unittest.TestCase):
+    """Tests of the configuration parser"""
+    
+    def test_config_has_content(self):
+        """Test that when we parse a standard-format config file, the
+        dictionary returned has non-zero-length content"""
+        
+        config_file_path = path.join('../../configs','obscontrol_config.xml')
+        config = config_parser.read_config(config_file_path)
+        self.assertTrue(len(config) > 0)
+
+if __name__ == '__main__':
+    unittest.main()
