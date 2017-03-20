683661c4e3797fb5f98660e16ef7323c46f15b1f
rachel3834 <rstreet@lcogt.net>
2017-03-20 15:02:23 +0100

    Adapting query_db functions from commands syntax back to standard python function syntax

---
+++ scripts/query_db.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 18:50:24 2017
+
+@author: rstreet
+"""
+import os
+import sys
+from local_conf import get_conf
+robonet_site = get_conf('robonet_site')
+sys.path.append(robonet_site)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'robonet_site.settings')
+from django.core import management
+from django.conf import settings
+from django.utils import timezone
+from django import setup
+from datetime import datetime, timedelta
+setup()
+
+from events.models import ObsRequest
+
+
+def get_active_obs(self):
+    """Function to extract a list of the currently-active observations 
+    requests from the database"""
+
+    qs = ObsRequest.objects.filter(
+                    time_expire__gt = datetime.utcnow() 
+                    ).exclude(
+                    timestamp__lte = datetime.utcnow()
+                    )
+    print qs
+    return qs
