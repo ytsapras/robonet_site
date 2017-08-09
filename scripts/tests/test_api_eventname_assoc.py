a08a4b217912ea038f0c0ae70b69ae79688e85f2
rachel3834 <rstreet@lcogt.net>
2017-08-08 23:31:48 -0700

    Correct details

--- scripts/tests/test_api_eventname_assoc.py
+++ scripts/tests/test_api_eventname_assoc.py
@@ -20,7 +20,7 @@ def test_api_eventname_assoc():
     params = {'name': 'OGLE-2017-BLG-1515',\
               'ev_ra': '17:28:03.84', 'ev_dec': '-29:01:25.20'}
     config = {'db_user_id': 'rstreet', \
-                'db_pswd': 'skynet1186'}
+                'db_pswd': 'xxx'}
     response = api_tools.check_eventname_assoc(config,params,testing=True)
     print(response)
     
