74d43e898ecb4303cd803f9e603e06900818e59c
rachel3834 <rstreet@lco.global>
2018-04-27 16:26:48 -0700

    Converted testing flag too Boolean

--- scripts/update_subrequest_status.py
+++ scripts/update_subrequest_status.py
@@ -42,7 +42,7 @@ def update_subrequest_status(look_back_days=1.0):
               'time_expire': end_date.strftime("%Y-%m-%dT%H:%M:%S"),
               'request_status': 'AC'}
               
-    obs_list = api_tools.get_obs_list(config,params,testing=config['testing'])
+    obs_list = api_tools.get_obs_list(config,params,testing=bool(config['testing'])
     
     log.info('Database reports '+str(len(obs_list))+' observation(s) within this timeframe')
     
